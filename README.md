ba29d59ea204c4577c2e8aedc9135f616d479bcd
Parmeet Singh <parmsingh9899@gmail.com>
2026-01-22 10:02:58 +0530

    This is the first project commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This the first linux project and practice.This project has:
+
+- Terminal usage
+- Python script
+- Git Workflow
+
---
+++ main.py
@@ -0,0 +1,9 @@
+import datetime
+
+def main():
+   today_datetime=datetime.datetime.now()
+   print("I want oo become ai engineer")
+   print(f"Today day is {today_datetime}")
+
+if __name__=="__main__":
+       main()
